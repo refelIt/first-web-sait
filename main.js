0f2edf3396ea19c2d1b8b50fc5f89c5b3f65cb31
refelIt <69353161+refelIt@users.noreply.github.com>
2020-08-21 16:35:11 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+new WOW().init();
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    pagination: {
+        el: '.projects-pagination',
+        bulletClass: 'projects-bullet',
+        bulletActiveClass: 'projects-bullet-active',
+    },
+});
